5e338f7ea479042c22df2f29ba499dcdfad0ecbe
Zoey <zoey@z0ey.de>
2023-10-22 18:59:05 +0200

    fix shellcheck
    Signed-off-by: Zoey <zoey@z0ey.de>

--- healthcheck.sh
+++ healthcheck.sh
@@ -4,11 +4,11 @@ if ! [ -f /tmp/IPv4.json ] && ! [ -f /tmp/IPv6.json ]; then
   exit 1
 fi
 
-if [ -f /tmp/IPv4.json ] && [ "$(cat /tmp/IPv4.json | jq -r .status)" != "success" ]; then 
+if [ -f /tmp/IPv4.json ] && [ "$(jq -r .status </tmp/IPv4.json)" != "success" ]; then 
   exit 1
 fi
 
-if [ -f /tmp/IPv6.json ] && [ "$(cat /tmp/IPv6.json | jq -r .status)" != "success" ]; then 
+if [ -f /tmp/IPv6.json ] && [ "$(jq -r .status </tmp/IPv6.json)" != "success" ]; then 
   exit 1
 fi
 
